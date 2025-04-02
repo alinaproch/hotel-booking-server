b66e8e3f83b4d31e2b4010795b830a7b1454e7b7
alinaproch <alinaproch15@gmail.com>
2025-04-02 22:32:32 +0500

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import { body } from "express-validator";
 import hotelRoutes from "./routes/hotel.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import bookingRoutes from "./routes/booking.routes.js";
-import validate from "./middlewares/validate.js"; 
+import validate from "./middlewares/validate.js";
 
 const app = express();
 app.use(express.json());
@@ -14,14 +14,15 @@ mongoose
   .then(() => console.log("MongoDB подключена"))
   .catch((err) => console.error("Ошибка подключения к MongoDB:", err));
 
-
 app.post(
   "/hotels",
   [
     body("name").isString().withMessage("Название отеля должно быть строкой"),
     body("location").isString().withMessage("Локация должна быть строкой"),
     body("price").isNumeric().withMessage("Цена должна быть числом"),
-    body("rooms").isInt({ min: 1 }).withMessage("Количество комнат должно быть ≥ 1"),
+    body("rooms")
+      .isInt({ min: 1 })
+      .withMessage("Количество комнат должно быть ≥ 1"),
     validate,
   ],
   (req, res) => {
@@ -34,7 +35,9 @@ app.post(
   [
     body("name").isString().withMessage("Имя должно быть строкой"),
     body("email").isEmail().withMessage("Некорректный email"),
-    body("password").isLength({ min: 6 }).withMessage("Пароль должен быть ≥ 6 символов"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Пароль должен быть ≥ 6 символов"),
     validate,
   ],
   (req, res) => {
@@ -48,3 +51,4 @@ app.use("/bookings", bookingRoutes);
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`));
+
