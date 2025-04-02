34404257aa431ed133417e386e3ea3863b7c958c
alinaproch <alinaproch15@gmail.com>
2025-04-02 23:46:55 +0500

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,38 +1,19 @@
 import express from "express";
 import { body } from "express-validator";
+import { createUser } from "../controllers/userController.js";
 import validate from "../middlewares/validate.js";
-import { createUser, updateUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.post(
   "/",
   [
-    body("name").isString().withMessage("Имя должно быть строкой"),
-    body("email").isEmail().withMessage("Email должен быть валидным"),
-    body("password")
-      .isLength({ min: 6 })
-      .withMessage("Пароль должен содержать минимум 6 символов"),
+    body("name").isString().isLength({ min: 3 }).withMessage("Имя должно быть не менее 3 символов"),
+    body("email").isEmail().withMessage("Некорректный email"),
+    body("password").isLength({ min: 6 }).withMessage("Пароль должен содержать минимум 6 символов"),
+    validate,
   ],
-  validate,
   createUser
-);
-
-router.patch(
-  "/:id",
-  [
-    body("name").optional().isString().withMessage("Имя должно быть строкой"),
-    body("email")
-      .optional()
-      .isEmail()
-      .withMessage("Email должен быть валидным"),
-    body("password")
-      .optional()
-      .isLength({ min: 6 })
-      .withMessage("Пароль должен содержать минимум 6 символов"),
-  ],
-  validate,
-  updateUser
 );
 
 export default router;
