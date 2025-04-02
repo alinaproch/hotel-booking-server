72c9b269e3a0c5b195d7ac91e166ec66a58c14d7
alinaproch <alinaproch15@gmail.com>
2025-04-02 23:44:23 +0500

    Update hotelController.js

--- controllers/hotelController.js
+++ controllers/hotelController.js
@@ -1,7 +1,24 @@
-export const createHotel = async (req, res) => {
-  try {
-    res.status(201).json({ message: "Отель создан!" });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+import express from "express";
+import { body } from "express-validator";
+import { createUser } from "../controllers/userController.js";
+import validate from "../middlewares/validate.js";
+
+const router = express.Router();
+
+router.post(
+  "/",
+  [
+    body("name")
+      .isString()
+      .isLength({ min: 3 })
+      .withMessage("Имя должно быть не менее 3 символов"),
+    body("email").isEmail().withMessage("Некорректный email"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Пароль должен содержать минимум 6 символов"),
+    validate,
+  ],
+  createUser
+);
+
+export default router;
