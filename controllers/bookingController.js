ada02876bd348df0da377280caa55f115cffc78b
alinaproch <alinaproch15@gmail.com>
2025-04-02 23:12:25 +0500

    Update bookingController.js

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,7 +1,34 @@
-export const createBooking = async (req, res) => {
-  try {
-    res.status(201).json({ message: "Бронирование создано!" });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
+import express from "express";
+import { body } from "express-validator";
+import bookingController from "../controllers/bookingController.js";
+import validate from "../middlewares/validate.js";
+
+const router = express.Router();
+
+
+router.post(
+  "/",
+  [
+    body("userId").isMongoId().withMessage("Некорректный ID пользователя"),
+    body("hotelId").isMongoId().withMessage("Некорректный ID отеля"),
+    body("startDate").isISO8601().withMessage("Дата начала должна быть в формате ISO8601"),
+    body("endDate").isISO8601().withMessage("Дата окончания должна быть в формате ISO8601"),
+    body("totalPrice").isFloat({ min: 0.01 }).withMessage("Цена должна быть больше 0"),
+    validate
+  ],
+  bookingController.createBooking
+);
+
+
+router.patch(
+  "/:id",
+  [
+    body("startDate").optional().isISO8601().withMessage("Дата начала должна быть в формате ISO8601"),
+    body("endDate").optional().isISO8601().withMessage("Дата окончания должна быть в формате ISO8601"),
+    body("totalPrice").optional().isFloat({ min: 0.01 }).withMessage("Цена должна быть больше 0"),
+    validate,
+  ],
+  bookingController.updateBooking
+);
+
+export default router;
